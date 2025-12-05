8245a68032fe67654c31b0d243c2726534d5964e
Paul Meisel <pmeisel@gmail.com>
2025-12-05 08:55:38 -0500

    introducing git

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import * as fs from "fs";
 import path from "path";
 import { defineConfig } from "vite";
+import type { ProxyOptions} from "vite";
 import {
 	API_KEY,
 	API_URL,
@@ -12,8 +13,33 @@ import {
 	DASHBOARD_SPEED_SECONDS,
 	DONE_STATUS,
 	VACATION_KEY,
+	GITKEY,
+	GITREPOS,
 } from "./globals";
 
+const git_proxies: {[key: string]: ProxyOptions} = {};
+const git_proxies_name_path: {[key: string]:string}  = {};
+GITREPOS.forEach((repo, index: number) => {
+	const repo_path = '/git_' + index;
+	const repo_name = repo.name;
+	const repo_target = repo.url.replace('https://github.com/', 'https://api.github.com/repos/');
+
+	git_proxies_name_path[repo_name] = repo_path;
+
+	git_proxies[repo_path] = {
+	    target: repo_target,
+	    changeOrigin: true,
+	    secure: false,
+	    headers: {
+	      Accept: 'application/vnd.github+json',
+	      Authorization: 'Bearer ' + GITKEY,
+	      'X-GitHub-Api-Version': '2022-11-28',
+	      'User-Agent': 'validator'
+	    },
+	    rewrite: (path) => path.replace(new RegExp(`^${repo_path}`), ""),
+	};
+});
+console.log(git_proxies);
 const ducks = fs.readdirSync("./src/assets/ducks/");
 // https://vite.dev/config/
 export default defineConfig({
@@ -28,6 +54,7 @@ export default defineConfig({
 		),
 		__DUCKS__: JSON.stringify(ducks),
 		__DASHBOARD_DUCKS__: JSON.stringify(DASHBOARD_DUCKS),
+		__GIT_REPOS__: JSON.stringify(git_proxies_name_path),
 	},
 	server: {
 		host: "0.0.0.0",
@@ -48,6 +75,7 @@ export default defineConfig({
 					});
 				},
 			},
+			...git_proxies
 		},
 		fs: {
 			deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**"],

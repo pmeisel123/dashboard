2c59584e50a6586d97ffb91a7adb8c235f4c2101
Paul Meisel <pmeisel@gmail.com>
2025-12-05 09:35:55 -0500

    Updating the loading bar for the dashboard

--- src/Components/Dashboard/progres.tsx
+++ src/Components/Dashboard/progres.tsx
@@ -3,11 +3,11 @@ import { styled } from "@mui/material/styles";
 declare const __DASHBOARD_SPEED_SECONDS__: number;
 
 export const Progress = styled(LinearProgress)(() => ({
-	position: 'fixed',
+	position: "fixed",
 	top: 0,
 	left: 0,
 	zIndex: 99999,
-	width: '100%',
+	width: "100%",
 	"& .MuiLinearProgress-bar": {
 		animationDuration: __DASHBOARD_SPEED_SECONDS__ + "s",
 	},

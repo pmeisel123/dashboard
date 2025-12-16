d74bcb0eefc39a9749a95825eb13f4353197f348
pmeisel123 <34077916+pmeisel123@users.noreply.github.com>
2025-12-16 07:30:13 -0500

    Update dashboard configuration constants
    Updated the HOST and API_URL constants for the dashboard configuration. Added API_CONFLUENCE_URL and improved comments for clarity.

--- globals.ts
+++ globals.ts
@@ -1,19 +1,19 @@
 import type { CustomFieldsObjectProps, DashboardsProps, ReposProps } from './src/Api/Types';
 
 // Domain you are running this dashboard on
-export const HOST = "www.host.net";
+export const HOST = "www.cybersquad.net";
 export const PORT = 3000;
 
 // Jira Settings
 // https://id.atlassian.com/manage-profile/security/api-tokens
 export const API_KEY = ''; // API Key for Jira
-export const API_URL = ''; // Host of jira Example: https://pmeisel.atlassian.net/
+export const API_URL = '/'; // URL fir Jira Example: https://pmeisel.atlassian.net/
+export const API_CONFLUENCE_URL = ''; // URL for Confluence Example: https://pmeisel.atlassian.net/wiki
 export const API_USERNAME = ''; // Email address used to create the jira api key
-export const VACATION_KEY: ('email' | 'name') = 'email';
-export const DONE_STATUS = ['Done'];
+export const VACATION_KEY: ('email' | 'name') = 'email'; // How Jira Users and vacation api link user (by name or by email)
+export const DONE_STATUS = ['Done']; // All the status for marking a ticket as closed
 export const CUSTOM_FIELDS: CustomFieldsObjectProps = {}; // Custom fields for Jira Ticket
 /* Example
-{
 	'customfield_10001': {
 		'Name': 'Team',
 		'Type': 'Text'
@@ -33,15 +33,17 @@ export const CUSTOM_FIELDS: CustomFieldsObjectProps = {}; // Custom fields for J
 // Git Settings
 // https://github.com/settings/personal-access-tokens
 // Grant read access to Commit Status, Contents, Metadata, pull requests
-export const GITTOKEN = ''; // Starts with github_
+export const GITTOKEN = ''; // starts with github_
 export const GITREPOS: ReposProps[] = [];
 /* Example
+[
 	{
 		name: 'Dashboard',
 		url: 'https://github.com/pmeisel123/dashboard/'
 	}
 ];
 */
+
 // Dashboards Settings
 export const DASHBOARD_SPEED_SECONDS = 10;
 export const DASHBOARDS: DashboardsProps = {
@@ -49,6 +51,32 @@ export const DASHBOARDS: DashboardsProps = {
 		key: 'dev',
 		name: "Dev Dashboard",
 		pages: [
+			{
+				name: "Front Page",
+				split: 'fourways',
+				pages : [
+				{
+					name: "Date",
+					url: "/date"
+				},
+				{
+					name: "Time",
+					url: "/time"
+				},
+				{
+					name: "Dev Dashboard Text",
+					url: "/text?text=Dev%20Dashboard"
+				},
+				{
+					name: "Next Holiday",
+					url: "/nextholiday"
+				},
+				]
+			},
+			{
+				name: "Test Confluence Page",
+				url: "/wiki?SpaceKey=~7120206538128356fe449cad24bec282ea0b50&PageId=393231",
+			},
 			{
 				name: "Recent Tickets 15 days",
 				url: "/RecentTickets?days=15"
@@ -75,6 +103,28 @@ export const DASHBOARDS: DashboardsProps = {
 		key: 'company',
 		name: "Company Dashboard",
 		pages: [
+			{
+				name: "Front Page",
+				split: 'fourways',
+				pages : [
+				{
+					name: "Date",
+					url: "/date"
+				},
+				{
+					name: "Time",
+					url: "/time"
+				},
+				{
+					name: "Company Dashboard Text",
+					url: "/text?text=Company%20Dashboard"
+				},
+				{
+					name: "Next Holiday",
+					url: "/nextholiday"
+				},
+				]
+			},
 			{
 				name: "Recent Tickets",
 				url: "/RecentTickets?days=15"

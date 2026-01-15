c962efc4767c759c02f0b51fd24b5c46ff8ac87f
Paul Meisel <pmeisel@gmail.com>
2026-01-15 10:15:11 -0500

    OPS-16 - Making the site use config.json instead of globals.ts

--- src/Pages/EditConfig/jira.tsx
+++ src/Pages/EditConfig/jira.tsx
@@ -64,7 +64,7 @@ export const EditJiraConfigTab: FC<{
 								setEditApiKey(true);
 							}}
 						>
-							Override current API key and name
+							Override current API Key and User
 						</Button>
 					</Grid>
 					<Grid size={{ xs: 12, md: 5 }}>
@@ -150,7 +150,7 @@ export const EditJiraConfigTab: FC<{
 			<TextField
 				id="ApiUrl"
 				value={apiUrl}
-				placeholder="https://pmeisel.atlassian.net/"
+				placeholder='https://pmeisel.atlassian.net/'
 				fullWidth
 				disabled={!apiKey || !userName}
 				helperText={!apiKey || !userName ? "Enter Api Key and username to update" : " "}
@@ -183,7 +183,7 @@ export const EditJiraConfigTab: FC<{
 				id="ApiConfluenceUrl"
 				value={apiConfluenceUrl}
 				fullWidth
-				placeholder="https://pmeisel.atlassian.net/wiki/"
+				placeholder='https://pmeisel.atlassian.net/wiki/'
 				disabled={!apiKey || !userName}
 				helperText={!apiKey || !userName ? "Enter Api Key and username to update" : " "}
 				onChange={(event) => {

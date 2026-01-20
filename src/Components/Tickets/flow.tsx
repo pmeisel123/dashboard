2f3dd1754d13a7ed911ffb463b3b9a42c754a9d3
Paul Meisel <pmeisel@gmail.com>
2026-01-20 10:24:29 -0500

    OPS-17: initial flow chart

--- src/Components/Tickets/flow.tsx
+++ src/Components/Tickets/flow.tsx
@@ -2,6 +2,7 @@ import { LayoutControls, LayoutProvider } from "@jalez/react-flow-automated-layo
 import { Link } from "@mui/material";
 import type { ConfigProps, TicketProps } from "@src/Api";
 import type { Edge, Node } from "@xyflow/react";
+import { useResizeObserver } from "../Misc";
 
 import type { Theme } from "@mui/material/styles";
 import {
@@ -12,57 +13,63 @@ import {
 	ReactFlowProvider,
 	useEdgesState,
 	useNodesState,
+	useReactFlow,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import type { FC } from "react";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { RenderEstimate } from "./const";
 
-export const TicketFlow: FC<{
+export const FlowInternal: FC<{
 	tickets: TicketProps[];
 	defaultEstimate: number | null;
 	config: ConfigProps;
 	theme: Theme;
 }> = ({ tickets, defaultEstimate, config, theme }) => {
+	const { fitView } = useReactFlow();
+	const [ref, size] = useResizeObserver<HTMLDivElement>();
+
 	let initialNodes: Node[] = [];
 	let groups: { [key: string]: { estimate: number; node: Node } } = {};
 	let initialEdges: Edge[] = [];
+	const createGroupNode = (group: string, match: string, ticket: TicketProps, parent: string) => {
+		if (!(group in groups)) {
+			let groupnode: Node = {
+				id: group,
+				data: { label: <></> },
+				position: { x: 0, y: 0 },
+				style: { backgroundColor: "rgba(75, 25, 75, 0.1" },
+			};
+			if (parent) {
+				groupnode.parentId = parent;
+			}
+			groups[group] = { node: groupnode, estimate: 0 };
+			initialNodes.push(groupnode);
+		}
+		const estimate = ticket.timeestimate != null ? ticket.timeestimate : defaultEstimate ? defaultEstimate : 0;
+		groups[group]["estimate"] += estimate;
+		groups[group]["node"].data.label = (
+			<>
+				{match}
+				<br />
+				Total Estimate: {groups[group]["estimate"]}
+			</>
+		);
+	};
 	tickets.forEach((ticket: TicketProps) => {
 		const id = ticket.key;
 		let group = "";
 		if (ticket.summary) {
 			const matches = ticket.summary.match(/(.*?):/g) || [];
 			if (matches.length) {
-				let level = 0;
 				matches.forEach((match: string) => {
-					level += 5;
 					const parent = group;
 					group += match;
-					const estimate =
-						ticket.timeestimate != null ? ticket.timeestimate : defaultEstimate ? defaultEstimate : 0;
-					if (!(group in groups)) {
-						let groupnode: Node = {
-							id: group,
-							data: { label: <></> },
-							position: { x: 0, y: 0 },
-							style: { backgroundColor: "rgba(50, 0, 50, " + level / 100 + ")" },
-						};
-						if (parent) {
-							groupnode.parentId = parent;
-						}
-						groups[group] = { node: groupnode, estimate: estimate };
-						initialNodes.push(groupnode);
-					} else {
-						groups[group]["estimate"] += estimate;
-					}
-					groups[group]["node"].data.label = (
-						<>
-							{match}
-							<br />
-							Estimate: {groups[group]["estimate"]}
-						</>
-					);
+					createGroupNode(group, match, ticket, parent);
 				});
+			} else {
+				group = "Uncategorized (Names without a ':')";
+				createGroupNode(group, group, ticket, "");
 			}
 		}
 
@@ -102,18 +109,29 @@ export const TicketFlow: FC<{
 					id: id + "-" + parent,
 					source: parent,
 					target: id,
-					markerEnd: { type: MarkerType.ArrowClosed },
+					markerEnd: {
+						height: 50,
+						type: MarkerType.ArrowClosed,
+						width: 50,
+					},
+					
 				});
 			});
 		}
 		initialNodes.push(node);
 	});
-	console.log(initialNodes);
-	if (!initialNodes.length) {
-		return null;
-	}
 	const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
 	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			fitView({
+				duration: 100,
+			});
+		}, 300);
+
+		return () => clearTimeout(timeout);
+	}, [fitView, nodes.length, size]);
 	const nodeIdWithNode = new Map<string, Node>();
 	nodes.forEach((node) => {
 		nodeIdWithNode.set(node.id, node);
@@ -132,49 +150,66 @@ export const TicketFlow: FC<{
 		[setEdges],
 	);
 
+	if (!initialNodes.length) {
+		return null;
+	}
+	return (
+		<LayoutProvider
+			initialDirection="RIGHT"
+			initialAutoLayout={true}
+			initialPadding={50}
+			initialSpacing={{ node: 80, layer: 80 }}
+			nodeIdWithNode={nodeIdWithNode}
+			updateNodes={updateNodesHandler}
+			updateEdges={updateEdgesHandler}
+		>
+			<div
+				style={{
+					width: "100%",
+					height: "75vh",
+					resize: "vertical",
+					border: "1px solid black",
+					overflow: "auto",
+				}}
+				ref={ref}
+			>
+				<ReactFlow
+					nodes={nodes}
+					edges={edges}
+					nodeOrigin={[0, 0]}
+					minZoom={0.001}
+					fitView
+					fitViewOptions={{
+						minZoom: 0.5,
+						maxZoom: 5,
+					}}
+					onNodesChange={onNodesChange}
+					onEdgesChange={onEdgesChange}
+					connectionLineType={ConnectionLineType.SmoothStep}
+				>
+					<Controls position="top-right">
+						<LayoutControls
+							showDirectionControls={true}
+							showAutoLayoutToggle={true}
+							showSpacingControls={true}
+							showApplyLayoutButton={true}
+						/>
+					</Controls>
+				</ReactFlow>
+			</div>
+		</LayoutProvider>
+	);
+};
+
+export const TicketFlow: FC<{
+	tickets: TicketProps[];
+	defaultEstimate: number | null;
+	config: ConfigProps;
+	theme: Theme;
+}> = (props) => {
 	return (
 		<ReactFlowProvider>
-			<LayoutProvider
-				initialDirection="DOWN"
-				initialAutoLayout={true}
-				initialPadding={50}
-				initialSpacing={{ node: 80, layer: 80 }}
-				nodeIdWithNode={nodeIdWithNode}
-				updateNodes={updateNodesHandler}
-				updateEdges={updateEdgesHandler}
-			>
-				<div style={{ width: "100%", height: "50vh" }}>
-					<ReactFlow
-						nodes={nodes}
-						edges={edges}
-						nodeOrigin={[0, 0]}
-						minZoom={0.001}
-						fitView
-						fitViewOptions={{
-							minZoom: 0.5,
-							maxZoom: 5,
-							padding: {
-								top: 0,
-								left: 0,
-								right: "95%",
-								bottom: "95%",
-							},
-						}}
-						onNodesChange={onNodesChange}
-						onEdgesChange={onEdgesChange}
-						connectionLineType={ConnectionLineType.SmoothStep}
-					>
-						<Controls position="top-right" showFitView={false}>
-							<LayoutControls
-								showDirectionControls={true}
-								showAutoLayoutToggle={true}
-								showSpacingControls={true}
-								showApplyLayoutButton={true}
-							/>
-						</Controls>
-					</ReactFlow>
-				</div>
-			</LayoutProvider>
+			<FlowInternal {...props} />
 		</ReactFlowProvider>
 	);
 };
